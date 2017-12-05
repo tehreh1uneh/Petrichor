7d0f3a0f99b09536ee4f9d7dc5bbda9076097758
eurhythmic <andrey.solovetov@gmail.com>
2017-12-05 22:18:49 +1000

    Initial commit

---
+++ app/src/main/java/com/tehreh1uneh/petrichor/restclient/RestClient.java
@@ -0,0 +1,31 @@
+package com.tehreh1uneh.petrichor.restclient;
+
+import android.app.Application;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.tehreh1uneh.petrichor.restclient.config.Config.BASE_URL;
+
+
+public class RestClient{
+
+    private static OpenWeatherApi api;
+
+    private static void createApi(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        api = retrofit.create(OpenWeatherApi.class);
+    }
+
+
+    public static OpenWeatherApi getApi() {
+        if (api == null){
+            createApi();
+        }
+        return api;
+    }
+}
---
+++ app/src/main/java/com/tehreh1uneh/petrichor/restclient/config/Config.java
@@ -0,0 +1,7 @@
+package com.tehreh1uneh.petrichor.restclient.config;
+
+public final class Config {
+    public static final String API_KEY_OPEN_WEATHER = "25dba57b32e5e71ebbdf4abda6b76b22";
+    public static final String BASE_URL = "https://api.openweathermap.org/";
+    public static final String GET_URL = "/data/2.5/weather";
+}
